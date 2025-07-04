2fb1318d0372c527f3adde81b431016acc0fe773
Toto571 <totomaldonado2005@gmail.com>
2025-07-04 18:31:02 -0300

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { dirname, resolve } from "node:path";
+import { resolve, dirname as pathDirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
-const dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = pathDirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(dirname, "index.html"),
-        favoritos: resolve(__dirname, "detalles/detalles.html"),
+        main: resolve(__dirname, "index.html"),
+        detalles: resolve(__dirname, "detalles/detalles.html"),
       },
     },
   },
